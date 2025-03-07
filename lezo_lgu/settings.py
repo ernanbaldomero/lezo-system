a3bd80d79a60a714082bf8abd05ef68de6fdb1ee
Ernan Baldomero <ernanbaldomero@users.noreply.github.com>
2025-03-07 12:59:33 +0800

    Update settings.py

--- lezo_lgu/settings.py
+++ lezo_lgu/settings.py
@@ -66,8 +66,12 @@ DATABASES = {
         'CONN_MAX_AGE': 600,
     },
     'test': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_lezo_db',
+        'USER': os.getenv('DB_USER', 'lezo_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'Lezo2025'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 

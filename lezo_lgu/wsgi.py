b12bfc0c4f3da050a28d820cedacad87d94ebe6c
Ernan Baldomero <ernanbaldomero@users.noreply.github.com>
2025-03-07 14:23:20 +0800

    Update wsgi.py

--- lezo_lgu/wsgi.py
+++ lezo_lgu/wsgi.py
@@ -1,1 +1,19 @@
+"""
+WSGI config for Lezo LGU System project.
 
+This module contains the WSGI application used by Gunicorn to serve the Django project.
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on WSGI, see:
+- https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+- https://www.python.org/dev/peps/pep-3333/
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the default Django settings module for the 'lezo_lgu' project
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lezo_lgu.settings')
+
+# Create the WSGI application callable
+application = get_wsgi_application()

028a07938c05f440abbb84ea1e61152ef48aa2a4
Ernan Baldomero <ernanbaldomero@users.noreply.github.com>
2025-03-07 12:30:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # Startup script for Lezo LGU System
-# Optimized for lightweight operation
-
 source venv/bin/activate
-gunicorn -b 0.0.0.0:8000 --workers 1 lezo_lgu.wsgi  # Single worker for 1GB RAM
+gunicorn -b 0.0.0.0:8000 --workers 1 lezo_lgu.wsgi

d969c9ac9b92ee5ea2ceb60d3265ccdd75648e76
Ernan Baldomero <ernanbaldomero@users.noreply.github.com>
2025-03-07 13:58:49 +0800

    Update sw.js

--- core/static/core/sw.js
+++ core/static/core/sw.js
@@ -1,1 +1,18 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('lezo-system-v1').then(cache => {
+            return cache.addAll([
+                '/citizens/',
+                '/static/core/sw.js'
+            ]);
+        })
+    );
+});
 
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
